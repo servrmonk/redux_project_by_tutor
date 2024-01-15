1eb46a79a9e07407e54de30a2f716af6974a6c1d
Raushan <roushankumarsingh2018@gmail.com>
2024-01-15 18:54:25 +0530

    incremented and decremented by 2

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,22 +1,37 @@
-import { useDispatch, useSelector } from "react-redux";
+import {  useDispatch, useSelector } from "react-redux";
 import classes from "./Counter.module.css";
 
-// useSelector or useStore to select the part of the state  in class based we will use connect
-
 const Counter = () => {
-  const counter = useSelector((state) => state.counter); //to extract the data from the store.when u use useSelector react redux will automaticallyset up the subscription to redux store for this component
-  const toggleCounterHandler = () => {};
   const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({type:'increment'})
+  };
+  const decrementHandler = () => {
+    dispatch({type:'decrement'})
+  };
+
+  const counter = useSelector((state) => state.counter);
+  const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <br />
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
-      <button onClick={() => dispatch({type:"increment"})}>IncrementBy5</button>
-      <button onClick={() => dispatch({type:"decrement"})}>DecrementBy5</button>
     </main>
   );
 };
 
+
+// in function component we use hook useDispatch and useSelector hook but hooks r not usable in class based component react redux also export a function connect u connect class based component  to redux actually u can also use this in function
+
+
+
+// export default connect(mapStateToProps,mapDispatchToProps)(Counter);
 export default Counter;
--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,11 @@ const counterReducer = (state = { counter: 0 }, action) => {
   switch (action.type) {
     case "increment":
       return {
-        counter: state.counter + 5,
+        counter: state.counter + 2,
       };
     case "decrement":
       return {
-        counter: state.counter - 5,
+        counter: state.counter - 2,
       };
 
     default:
