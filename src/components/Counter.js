a4fbeae4879a00ded0d46fbd30ea92bd4b2c2a12
Raushan <roushankumarsingh2018@gmail.com>
2024-01-15 15:00:56 +0530

    increment and decrement by 5 done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import classes from "./Counter.module.css";
 
 // useSelector or useStore to select the part of the state  in class based we will use connect
@@ -6,13 +6,15 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   const counter = useSelector((state) => state.counter); //to extract the data from the store.when u use useSelector react redux will automaticallyset up the subscription to redux store for this component
   const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
-      
+      <button onClick={() => dispatch({type:"increment"})}>IncrementBy5</button>
+      <button onClick={() => dispatch({type:"decrement"})}>DecrementBy5</button>
     </main>
   );
 };
--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,11 @@ const counterReducer = (state = { counter: 0 }, action) => {
   switch (action.type) {
     case "increment":
       return {
-        counter: state.counter + 1,
+        counter: state.counter + 5,
       };
     case "decrement":
       return {
-        counter: state.counter - 1,
+        counter: state.counter - 5,
       };
 
     default:
