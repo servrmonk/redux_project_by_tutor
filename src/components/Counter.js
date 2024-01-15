896167fa37b1f09d1b9f26568b11de76d3ca26fa
Raushan <roushankumarsingh2018@gmail.com>
2024-01-15 14:52:57 +0530

    added useSelector and extract the value of count

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,18 @@
-import classes from './Counter.module.css';
+import { useSelector } from "react-redux";
+import classes from "./Counter.module.css";
+
+// useSelector or useStore to select the part of the state  in class based we will use connect
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter); //to extract the data from the store.when u use useSelector react redux will automaticallyset up the subscription to redux store for this component
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      
     </main>
   );
 };
